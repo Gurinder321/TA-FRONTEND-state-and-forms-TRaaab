3083ec713d419a314cead93a0a3a0ac68af2a7a4
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-06-21 11:38:56 -0700

    DONE

---
+++ block-BRaabw/small-shopping-cart/src/data.js
@@ -0,0 +1,27 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Macbook1',
+      price: 1400,
+      image:
+        'https://images.unsplash.com/photo-1638913658179-18c9a9c943f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      id: '2',
+      name: 'Macbook2',
+      price: 2400,
+      image:
+        'https://images.unsplash.com/photo-1638913658179-18c9a9c943f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      id: '3',
+      name: 'Macbook3',
+      price: 3400,
+      image:
+        'https://images.unsplash.com/photo-1638913658179-18c9a9c943f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60',
+    },
+  ],
+};
+
+export default data;
